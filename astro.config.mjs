819c07ab3a5689d3c521e9ce5eb5cc9c33f54f50
Markus Sommer <markussom@gmail.com>
2024-02-21 21:53:26 +0100

    feat: add compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import Icons from "unplugin-icons/vite";
 import tailwind from "@astrojs/tailwind";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), compress()],
   vite: {
     plugins: [Icons({
       compiler: "astro"
