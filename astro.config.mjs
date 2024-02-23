88d37f4a5cb3d441a624ca5c2f03692d770c8d74
Markus Sommer <markussom@gmail.com>
2024-02-23 14:13:51 +0100

    feat: add crit css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import Icons from "unplugin-icons/vite";
 import tailwind from "@astrojs/tailwind";
 
+import critters from "astro-critters";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), critters()],
   vite: {
     plugins: [Icons({
       compiler: "astro"
