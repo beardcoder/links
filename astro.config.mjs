7cf69a6df8826225c47d206bb8d49b35c236b268
Markus Sommer <markussom@gmail.com>
2024-02-23 14:13:11 +0100

    Revert "feat: add compress"
    This reverts commit 819c07ab3a5689d3c521e9ce5eb5cc9c33f54f50.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config";
 import Icons from "unplugin-icons/vite";
 import tailwind from "@astrojs/tailwind";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind()],
   vite: {
     plugins: [Icons({
       compiler: "astro"
