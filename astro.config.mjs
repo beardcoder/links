8fb34bc7eb158bbe4227d3177a9d6e0f3009242e
Markus Sommer <markussom@gmail.com>
2024-02-23 14:10:06 +0100

    Revert "feat: add crit css"
    This reverts commit 02120194a4e89c7d0e165065a25232abe4313e12.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import Icons from "unplugin-icons/vite";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 
-import critters from "astro-critters";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(), critters()],
+  integrations: [tailwind(), compress()],
   vite: {
     plugins: [Icons({
       compiler: "astro"
